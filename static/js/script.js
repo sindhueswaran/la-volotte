3535145367e179f4108db32cda64540d5c195b39
sindhueswaran <54274176+sindhueswaran@users.noreply.github.com>
2021-01-31 15:40:50 +0530

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -35,11 +35,43 @@
   })
 
 
+  // gallery slider
+  $('.gallery-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-arrow-left\'></i></button>',
+    nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-arrow-right\'></i></button>',
+    autoplay: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 401,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
   // testimonial slider
   $('.testimonial-slider').slick({
     dots: false,
     infinite: true,
-    speed: 300,
+    speed: 300, 
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
@@ -90,3 +122,5 @@
 
 
 })(jQuery);
+
+ 
